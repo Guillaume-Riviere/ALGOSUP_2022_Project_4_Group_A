327c539a08120732e3fd410e0716638ce321c808
Guillaume-Riviere <91249622+Guillaume-Riviere@users.noreply.github.com>
2022-04-04 15:56:43 +0200

    Test PNJ.cs

--- Unity/Devbox_B3/Devbox_B3/Assets/PNJ.cs
+++ Unity/Devbox_B3/Devbox_B3/Assets/PNJ.cs
@@ -6,40 +6,32 @@ using UnityEngine.AI;
 public class PNJ : MonoBehaviour
 {
     public GameObject PnjShelf;
-    public GameObject Pnj;
+    public GameObject PnjObject;
     public GameObject ProjectRooms;
     public GameObject MeetingRooms;
+    public GameObject amphi;
+    public GameObject leave;
+
+
+
+
+    private GameObject pnj;
+    private int random;
+    private Transform destination_transform;
+    private GameObject destination;
+    
+    private float randomX;
+    private float randomZ;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject pnj1;
-        int random;
-        Transform destination;
-        GameObject destination2;
         var count = ProjectRooms.transform.childCount;
-
-
-        int counter = 160;
-        float randomX;
-        float randomZ;
-
+        int counter = 6;
 
         for (int i = 0; i < counter; i++)
         {
-            randomX = Random.Range(-20f, 20f);
-            randomZ = Random.Range(-20f, 20f);
-            random = Random.Range(0, count);
-            destination = ProjectRooms.transform.GetChild(random);
-            destination2 = destination.gameObject;
-            pnj1 = Instantiate(Pnj) as GameObject;
-            pnj1.transform.name = "Pnj " + i;
-            pnj1.transform.parent = PnjShelf.transform;
-            pnj1.transform.localPosition = new Vector3(randomX, 0, randomZ);
-            pnj1.transform.rotation = Quaternion.identity;
-            pnj1.AddComponent<NavMeshAgent>();
-            var script = pnj1.AddComponent<Pathfinder>();
-            script.destination = destination2;
+            StartCoroutine(Personae_A(count, i, amphi, leave));
         }
         
     }
@@ -48,5 +40,39 @@ public class PNJ : MonoBehaviour
     void Update()
     {
         
+    }
+    IEnumerator Personae_A(int count, int i, GameObject amphi, GameObject leave)
+    {
+        randomX = Random.Range(-20f, 20f);
+        randomZ = Random.Range(-20f, 20f);
+
+
+        random = Random.Range(0, count);
+        destination_transform = ProjectRooms.transform.GetChild(random);
+
+
+        destination = destination_transform.gameObject;
+        pnj = Instantiate(PnjObject) as GameObject;
+        pnj.transform.name = "Pnj " + i;
+        pnj.transform.parent = PnjShelf.transform;
+        pnj.transform.localPosition = new Vector3(randomX, 0, randomZ);
+        pnj.transform.rotation = Quaternion.identity;
+        pnj.AddComponent<NavMeshAgent>();
+        var script = pnj.AddComponent<Pathfinder>();
+        script.destination = destination;
+
+
+
+        yield return new WaitForSeconds(120);
+
+        destination.transform.SetParent(amphi.transform);
+        destination.transform.SetPositionAndRotation(new Vector3(0, 0, 0), Quaternion.identity);
+
+
+        yield return new WaitForSeconds(120);
+
+        destination.transform.SetParent(leave.transform);
+        destination.transform.SetPositionAndRotation(new Vector3(0, 0, 0), Quaternion.identity);
+
     }
 }
